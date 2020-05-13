65741d2b46c3c4975d1f70c9fedeccc82f665ace
mazrou <ga_mazrou@esi.dz>
2020-05-13 17:41:12 +0000

    fixed bugs

--- app/src/main/java/com/example/dectionnaire/ListViewAdapter.java
+++ app/src/main/java/com/example/dectionnaire/ListViewAdapter.java
@@ -1,6 +1,8 @@
 package com.example.dectionnaire;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.speech.tts.TextToSpeech;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -8,82 +10,61 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 import java.util.Locale;
 
-public class ListViewAdapter extends BaseAdapter  {
+public class ListViewAdapter extends RecyclerView.Adapter<ListViewAdapter.ViewHolder> {
 
-    Context context;
+   private Context context;
     private static final String myPreference = "Mypreference";
     private static final int VR_REQUEST = 999;
 
     private int MY_DATA_CHECK_CODE = 0;
 
-    public TextToSpeech tts;
-    ArrayList<Word> wordLists;
+    private TextToSpeech tts;
+    private OnWordListener onWordListener ;
+    private ArrayList<Word> wordLists;
 
 
 
-    public ListViewAdapter(Context context, ArrayList<Word> words) {
+    public ListViewAdapter(Context context, ArrayList<Word> words , OnWordListener onWordListener) {
         this.context = context;
         this.wordLists = words;
+        this.onWordListener=onWordListener;
 
-    }
-
-    @Override
-    public int getCount() {
-
-        return wordLists.size();
-    }
-
-    @Override
-    public Object getItem(int position) {
-        return wordLists.get(position);
     }
 
     private void speakOut(String text) {
-
-
         tts.speak(text,TextToSpeech.QUEUE_FLUSH,null,text)  ;
 
-
     }
 
-    public class ViewHolder {
-        TextView eng_word, bang_word;
-        ImageButton imageButton;
+    public interface OnWordListener{
+        void onClickListener(int position);
+    }
 
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.e2b_list_view,parent,false);
+        return new ViewHolder(view, onWordListener);
     }
 
     @Override
-    public long getItemId(int position) {
-        return wordLists.indexOf(getItem(position));
-    }
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
 
 
-
-    @Override
-    public View getView(final int position, View convertView, final ViewGroup parent) {
-        LayoutInflater inflater = (LayoutInflater) context
-                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        final ViewHolder holder;
-        if (convertView == null) {
-            holder = new ViewHolder();
-            convertView = inflater.inflate(R.layout.e2b_list_view, null);
-            holder.eng_word = (TextView) convertView.findViewById(R.id.view_eng);
-            holder.bang_word = (TextView) convertView.findViewById(R.id.view_bang);
-            holder.imageButton = (ImageButton) convertView.findViewById(R.id.soundButton);
-            convertView.setTag(holder);
-
-        } else {
-            holder = (ViewHolder) convertView.getTag();
-        }
+        holder.bang_word.setText( wordLists.get(position).getBangWord());
         holder.eng_word.setText(wordLists.get(position).getEngWord());
-        holder.bang_word.setText(wordLists.get(position).getBangWord());
-
         holder.imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -92,7 +73,7 @@ public class ListViewAdapter extends BaseAdapter  {
                     public void onInit(int status) {
                         if (status == TextToSpeech.SUCCESS){
                             int result = tts.setLanguage(Locale.ENGLISH);
-                          String word_eng = wordLists.get(position).getEngWord();
+                            String word_eng = wordLists.get(position).getEngWord();
                             result = tts.setLanguage(Locale.ENGLISH);
 
                             String toSpeak = word_eng;
@@ -106,9 +87,37 @@ public class ListViewAdapter extends BaseAdapter  {
             }
         });
 
-        return convertView;
+    }
+
+    @Override
+    public int getItemCount() {
+        return wordLists.size();
     }
 
 
+public  class ViewHolder extends RecyclerView.ViewHolder  implements View.OnClickListener
+
+{
+    TextView eng_word, bang_word;
+    ImageButton imageButton;
+    OnWordListener onWordListener ;
+
+     ViewHolder(@NonNull View itemView , OnWordListener onWordListener) {
+        super(itemView);
+        this.eng_word = (TextView) itemView.findViewById(R.id.view_eng);
+        this.bang_word =(TextView) itemView.findViewById(R.id.view_bang);
+        this.imageButton = (ImageButton) itemView.findViewById(R.id.soundButton);
+        this.onWordListener = onWordListener;
+        itemView.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        onWordListener.onClickListener(getAdapterPosition());
+    }
 
 }
+
+
+}
+
+
--- app/src/main/java/com/example/dectionnaire/MainActivity.java
+++ app/src/main/java/com/example/dectionnaire/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.dectionnaire;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.Activity;
 import android.content.DialogInterface;
@@ -10,13 +12,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
-    ListView lv;
+public class MainActivity extends AppCompatActivity implements ListViewAdapter.OnWordListener {
+    RecyclerView lv;
     private boolean isChackBookmarks = false;
     ArrayList<Word> wordList=new ArrayList<Word>();;
     ListViewAdapter listAdapter;
@@ -24,7 +27,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        lv = (ListView) findViewById(R.id.dictionaryList);
+        lv = (RecyclerView) findViewById(R.id.dictionaryList);
+        initilizeList();
+        setAdapter();
+
+    }
+
+
+
+
+
+    private void setAdapter(){
+
+        lv.setLayoutManager(new LinearLayoutManager(this));
+        listAdapter=new ListViewAdapter(this, wordList ,this);
+        System.out.println(wordList.size());
+
+        lv.setAdapter(listAdapter);
+    }
+
+    private void initilizeList(){
 
         wordList.add( new Word("houce","nom féminin",
                 "La houce est un vêtement au Moyen Age qui ressemblait à une robe et qui se portait par-dessus les vêtements.",1));
@@ -57,20 +79,11 @@ public class MainActivity extends AppCompatActivity {
 
         wordList.add( new Word("hypocrite","adjectif, nom","Qui relève de l'hypocrisie, qui dissimule ses sentiments, " +
                 "ses pensées, sa véritable personnalité, qui est fourbe et sournois.",10));
-         listAdapter=new ListViewAdapter(this,wordList);
-        lv.setAdapter(listAdapter);
-        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView adapterView, View view, int i, long l) {
-               Toast.makeText(getApplicationContext(),"ddddddddd",Toast.LENGTH_LONG).show();
-                Intent intent = new Intent(getApplicationContext(),TextSpeelingActivity.class);
-
-                startActivity(intent);
-
-            }
-        });
     }
-    public void openAlert() {
+
+
+
+   /* public void openAlert() {
         AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
 
         builder.setTitle("Add Bookmarks");
@@ -97,7 +110,14 @@ public class MainActivity extends AppCompatActivity {
         AlertDialog alertDialog = builder.create();
 
         alertDialog.show();
+    }*/
+
+
+    @Override
+    public void onClickListener(int position) {
+        Intent intent = new Intent(getApplicationContext(),TextSpeelingActivity.class);
+        intent.putExtra("wordDef", wordList.get(position).getDef());
+        startActivity(intent);
+
     }
-
-
 }
